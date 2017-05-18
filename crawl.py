0a44c63f2c7307a2e05483dbac061598b389e133
Koury Lape <lapek@denison.edu>
2017-05-18 15:39:10 -0400

    back again

--- crawl.py
+++ crawl.py
@@ -130,7 +130,8 @@ if __name__ == "__main__":
                 {
                     "fallback": subject,
                     "color": "#36a64f",
-                    "text": "Quick Cache Warmer - Warmed %i pages in %i minutes on %s://%s" % (
+                    "pretext": "Quick Cache Warmer",
+                    "text": "Warmed %i pages in %i minutes on %s://%s" % (
                         num_warmed, finished, protocol, domain
                     )
                 }
